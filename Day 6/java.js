c0da699eff0b4fd4c59da9ad8ab6572b0bdbb55a
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-05-16 04:56:52 +0300

    Add files via upload

---
+++ Day 6/java.js
@@ -0,0 +1,16 @@
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+
+    const clockElement = document.getElementById('clock');
+    clockElement.textContent = timeString;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Run updateClock() once immediately to avoid initial delay
+updateClock();
